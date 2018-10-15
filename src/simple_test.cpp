b8cd2d4e7e41a16cee2eae14d0a72f326f1c9f74
Joshua Koch <jkoch@efcsystems.com>
2018-10-15 10:12:35 -0500

    Add better simple test between standard and safe comparison

--- src/simple_test.cpp
+++ src/simple_test.cpp
@@ -14,9 +14,8 @@ int main(
     try
     {
         safe_compare::greater< int, unsigned int > safe_greater;
-        std::cout << safe_greater( -1, 1 ) << std::endl;
-        std::cout << safe_greater( 1, 1 ) << std::endl;
-        std::cout << safe_greater( 2, 1 ) << std::endl;
+        std::cout << "Standard compare: " << ( -1 > 1U ) << std::endl;
+        std::cout << "Safe compare: " << safe_greater( -1, 1U ) << std::endl;
     }
     catch( std::exception const& e )
     {
